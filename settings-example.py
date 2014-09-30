a6a1ddd9f4b87dd4c3c0691c95ebf8cad491ac72
Jonas Friedmann <j@frd.mn>
2014-09-30 23:36:01 +0200

    Add settings file

---
+++ settings-example.py
@@ -0,0 +1,9 @@
+# Reddit bot credentials
+bot_username = "username"
+bot_password = "password"
+
+# Currency keywords to observe
+currency_keywords = ['nok', 'norwegian kroner']
+
+# Subreddits to observe
+observed_subreddits = ['news', 'pics']
